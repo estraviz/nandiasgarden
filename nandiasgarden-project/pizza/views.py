e16b808d6d87934d3db9e89d57a716e671fece10
Javier Estraviz <javier.estraviz@gmail.com>
2023-08-31 19:10:29 +0200

    Replaced form with what has been defined in the PizzaForm class

--- nandiasgarden-project/pizza/views.py
+++ nandiasgarden-project/pizza/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
+from .forms import PizzaForm
+
 
 def home(request):
     return render(request, 'pizza/home.html')
 
+
 def order(request):
-    return render(request, 'pizza/order.html')
+    form = PizzaForm()
+    return render(request, 'pizza/order.html', {'pizzaform': form})
