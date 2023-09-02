a7885bbc0e1e210d0311fb8429f96a57b80ffe92
Javier Estraviz <javier.estraviz@gmail.com>
2023-09-02 22:17:32 +0200

    After testing local validations for email and url fields in forms

--- nandiasgarden-project/pizza/forms.py
+++ nandiasgarden-project/pizza/forms.py
@@ -28,6 +28,8 @@ class PizzaForm(forms.ModelForm):
     #     )
 
     # image = forms.ImageField()
+    # email = forms.EmailField()
+    # url = forms.URLField()
 
     class Meta:
         model = Pizza
