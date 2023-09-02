4849c7eb69440494f1ea6043e41efb357bc8dcc9
Javier Estraviz <javier.estraviz@gmail.com>
2023-09-02 22:11:25 +0200

    After working with formsets, editing objects, input confirmation

--- nandiasgarden-project/pizza/forms.py
+++ nandiasgarden-project/pizza/forms.py
@@ -34,3 +34,7 @@ class PizzaForm(forms.ModelForm):
         fields = ['topping1', 'topping2', 'size']
         labels = {'topping1': 'Topping 1', 'topping2': 'Topping 2'}
         # widgets = {'size': forms.CheckboxSelectMultiple}
+
+
+class MultiplePizzaForm(forms.Form):
+    number = forms.IntegerField(min_value=2, max_value=6)
